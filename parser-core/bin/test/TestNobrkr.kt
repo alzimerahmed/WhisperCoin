7bfeaacd0e2c99799be7a684eb98b19d18d265e2
Alzimer Ahmed <alzimerahmed84@gmail.com>
2026-09-09 11:25:39 +0530

    chore: remove build artifact parser-core/bin, delete TODO.md, ignore bin/

--- parser-core/bin/test/TestNobrkr.kt
+++
@@ -1,8 +0,0 @@
-import com.alzimer.whispercoin.parser.core.bank.BankParserFactory
-
-
-fun main() {
-    val sender = "JK-NOBRKR-S"
-    val parser = BankParserFactory.getParser(sender)
-    println("Parser for $sender: ${parser?.getBankName() ?: "None"}")
-}
